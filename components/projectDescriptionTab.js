670b421334c6e8c58268b5a0dc863f024f86d918
BLake J <bjohnson293@ufl.edu>
2019-06-01 11:30:08 -0400

    progress on projects page creating rows up tabs

---
+++ components/projectDescriptionTab.js
@@ -0,0 +1,14 @@
+import React from "react"
+import ViewMoreButton from "../components/viewMoreButton.js"
+import "./styles/projectDescriptionTab.css"
+
+const ProjectDescriptionTab = (props) => (
+    <div className="tab_Grid_Container">
+        <div className="tab_Button">button</div>
+        <div className="tab_Name">name</div>
+        <div className="tab_Description">description</div>
+        <div className="tab_Icon">icon</div>
+    </div>
+)
+
+export default ProjectDescriptionTab
--- pages/projects.js
+++ pages/projects.js
@@ -3,7 +3,8 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-
+import ProjectDescriptionTab from "../components/projectDescriptionTab"
+ 
 import "./styles/projects.css"
 
 const Projects = () => (
@@ -12,6 +13,12 @@ const Projects = () => (
       <div className="project_Header">
           <h1>Projects</h1>
           <p>These projects are a combination of course work, personal ventures, and practice exercises for myself. Click the Github icon to be taken to the source code’s repository.  Course work projects do not have links to prevent current students at UF from discovering solutions. </p>
+      </div>
+      <div className="project_Grid_Wrapper">
+        <ProjectDescriptionTab />
+        <ProjectDescriptionTab />
+        <ProjectDescriptionTab />
+        <ProjectDescriptionTab />
       </div>
     </Layout>
 )
