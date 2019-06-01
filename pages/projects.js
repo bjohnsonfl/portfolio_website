714a1aa854ea4c5690d360a94be85f314aa4f3a4
BLake J <bjohnson293@ufl.edu>
2019-05-31 20:57:30 -0400

    work started on /projects page

---
+++ pages/projects.js
@@ -0,0 +1,19 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+import "./styles/projects.css"
+
+const Projects = () => (
+    <Layout>
+      <SEO title="Home" />
+      <div className="project_Header">
+          <h1>Projects</h1>
+          <p>These projects are a combination of course work, personal ventures, and practice exercises for myself. Click the Github icon to be taken to the source code’s repository.  Course work projects do not have links to prevent current students at UF from discovering solutions. </p>
+      </div>
+    </Layout>
+)
+
+export default Projects
